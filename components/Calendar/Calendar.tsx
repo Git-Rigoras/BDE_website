444c280a7e3ad149917944f43c4aa5b5e85d565d
INF-Zenyth <samuel.ward@teaminfernal.fr>
2022-11-07 20:11:13 +0100

    wip

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,5 +1,36 @@
-const Calendar = () => {
-  return <div></div>;
+import CalendarDates from "calendar-dates";
+
+const calendarDates = new CalendarDates();
+
+const days = [
+  "Dimanche",
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi",
+  "Samedi",
+];
+
+type CalendarDay = {
+  date: number;
+  iso: string;
+  type: string;
+};
+
+const Calendar = async () => {
+  const dates: Array<CalendarDay> = await calendarDates.getDates(new Date());
+  console.log(dates);
+
+  return (
+    <div className="grid grid-cols-7">
+      {dates.map((date, index) => (
+        <div key={index}>
+          {date.type} {date.iso}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Calendar;
